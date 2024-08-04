c694d16e065aff4e25e668be8ff1c712dfbc96b4
Kevin Zhang <kevin@Kevins-MBP.wlan>
2024-08-03 21:29:11 -0400

    c++ properties.json

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,23 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
-    SDL_Window *window;                    // Declare a pointer
-    SDL_Init(SDL_INIT_VIDEO);              // Initialize SDL2
-    // Create an application window with the following settings:
-    window = SDL_CreateWindow(
-        "An SDL2 window",                  // window title
-        SDL_WINDOWPOS_UNDEFINED,           // initial x position
-        SDL_WINDOWPOS_UNDEFINED,           // initial y position
-        640,                               // width, in pixels
-        480,                               // height, in pixels
-        SDL_WINDOW_OPENGL                  // flags - see below
-    );
-    // Check that the window was successfully made
-    if (window == NULL) {
-        // In the event that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
-    // The window is open: enter program loop (see SDL_PollEvent)
-    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
-    // Close and destroy the window
-    SDL_DestroyWindow(window);
-    // Clean up
-    SDL_Quit();
-    return 0;
-}
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
-// clang++ -std=c++11 -stdlib=libc++ src/main.cpp -I/opt/homebrew/Cellar/sdl2/2.30.5/include -L/opt/homebrew/Cellar/sdl2/2.30.5/lib -lSDL2 -o programfile
+int main(int argc, char* args[]) 
+{
+    
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow(
+        "Aquarium", 
+        SDL_WINDOWPOS_UNDEFINED, 
+        SDL_WINDOWPOS_UNDEFINED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        SDL_WINDOW_SHOWN
+    );
+
+
+    return 0;
+    
+}
