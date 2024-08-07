6e03ca39eeae5261185a39be8c20aa00a04f499d
Kevin Zhang <kevinzhang@Kevins-MBP.wlan>
2024-08-06 23:57:15 -0400

    Configured tasks.json

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,23 @@
-#include "SDL2/SDL.h"
+#include <iostream>
+#include "screen.h"
+#include "vector_math.h"
 
-using namespace std;
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
+Screen *screen = nullptr;
 int main(int argc, char *args[])
 {
-    int flags = SDL_WINDOW_SHOWN;
-    SDL_Window *window;
-    SDL_Renderer *renderer;
+    screen = new Screen(SCREEN_WIDTH, SCREEN_HEIGHT);
 
-    bool isRunning = SDL_Init(SDL_INIT_VIDEO) == 0;
-    if (isRunning)
-    {
-        window = window = SDL_CreateWindow(
-            "Aquarium",
-            SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            800,
-            600,
-            flags);
-
-        renderer = SDL_CreateRenderer(window, -1, 0);
-        (renderer) && SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    while (screen->Running()) {
+        screen->HandleEvents();
+        screen->Update();
+        screen->Render();
     }
 
-    while (isRunning)
-    {
-
-        SDL_Event event;
-        SDL_PollEvent(&event);
-
-        switch (event.type)
-        {
-        case SDL_QUIT:
-            isRunning = false;
-            break;
-
-        default:
-            break;
-        }
-
-        SDL_RenderClear(renderer);
-        SDL_RenderPresent(renderer);
-        SDL_UpdateWindowSurface(window);
-    }
-
-    SDL_DestroyWindow(window);
-    SDL_DestroyRenderer(renderer);
-    SDL_Quit();
+    screen->Clean();
+    delete screen;
     return 0;
 }
